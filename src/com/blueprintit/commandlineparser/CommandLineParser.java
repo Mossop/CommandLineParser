7c13df4c11e8dad9b53a4d6a7bd28a459968cfae
Dave Townsend <dtownsend@oxymoronical.com>
2004-04-04 10:33:29 +0000

    Initial commit for new project

---
+++ src/com/blueprintit/commandlineparser/CommandLineParser.java
@@ -0,0 +1,15 @@
+package com.blueprintit.commandlineparser;
+
+/**
+ * @author Dave
+ */
+public class CommandLineParser
+{
+	public CommandLineParser()
+	{
+	}
+	
+	public void parse(String[] args)
+	{
+	}
+}
